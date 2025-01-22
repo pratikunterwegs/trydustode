6b166fbffc067cc3584f7708440b7e3f897e308e
Pratik Gupte <pratikgupte16@gmail.com>
2025-01-22 10:01:16 +0000

    Add package function namespacing

--- touchstone/script.R
+++ touchstone/script.R
@@ -11,7 +11,7 @@ touchstone::branch_install()
 # benchmark a function call from your package (two calls per branch)
 touchstone::benchmark_run(
   # expr_before_benchmark = source("dir/data.R"), #<-- TODO OTPIONAL setup before benchmark
-  random_test = f(), #<- TODO put the call you want to benchmark here
+  random_test = trydustode::f(), #<- TODO put the call you want to benchmark here
   n = 2
 )
 
