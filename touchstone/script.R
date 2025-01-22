690c189aae9289793780cb08d877f4a77211d7c6
Pratik Gupte <pratikgupte16@gmail.com>
2025-01-22 09:41:14 +0000

    Benchmarking script has working code

--- touchstone/script.R
+++ touchstone/script.R
@@ -11,7 +11,7 @@ touchstone::branch_install()
 # benchmark a function call from your package (two calls per branch)
 touchstone::benchmark_run(
   # expr_before_benchmark = source("dir/data.R"), #<-- TODO OTPIONAL setup before benchmark
-  random_test = yourpkg::f(), #<- TODO put the call you want to benchmark here
+  random_test = stats::runif(10), #<- TODO put the call you want to benchmark here
   n = 2
 )
 
