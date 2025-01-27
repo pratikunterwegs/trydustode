5893dea87b9f56c6eeaff34db615bc6445ded668
Pratik Gupte <pratikgupte16@gmail.com>
2025-01-27 13:46:29 +0000

    Reading strata dims from

--- inst/dust/code.cpp
+++ inst/dust/code.cpp
@@ -1,4 +1,7 @@
+#include <cpp11.hpp>
+#include <cpp11eigen.hpp>
 #include <dust2/common.hpp>
+#include <iterator>
 
 // [[dust2::class(sirode)]]
 // [[dust2::time_type(continuous)]]
@@ -21,6 +24,7 @@ public:
     real_type beta;
     real_type sigma;
     real_type gamma;
+    size_t n_strata;
   };
 
   /// @brief Internal state - unclear purpose.
@@ -33,12 +37,19 @@ public:
 
   // unclear whether dust2/common.hpp links to monty - probably
   using rng_state_type = monty::random::generator<real_type>;
-  
+
   /// @brief How compartments are packed.
   /// @param shared A `shared_state` object, unclear why needed.
   /// @return A custom packing specification object.
-  static dust2::packing packing_state(const shared_state& shared) {
-    return dust2::packing{{"S", {}}, {"E", {}}, {"I", {}}, {"R", {}}, {"cases_inc", {}}};
+  static dust2::packing packing_state(const shared_state &shared) {
+    // PASS STRATA DATA TO `shared` AND READ TO CREATE VECTORS FOR
+    // PACKING VECTOR
+    const std::vector<size_t> N_STRATA(1, shared.n_strata);
+    return dust2::packing{{"S", N_STRATA},
+                          {"E", N_STRATA},
+                          {"I", N_STRATA},
+                          {"R", N_STRATA},
+                          {"cases_inc", N_STRATA}};
   }
 
   /// @brief Initialise shared parameters.
@@ -50,24 +61,26 @@ public:
     const real_type beta = dust2::r::read_real(pars, "beta", 0.2);
     const real_type sigma = dust2::r::read_real(pars, "sigma", 0.2);
     const real_type gamma = dust2::r::read_real(pars, "gamma", 0.1);
-    return shared_state{N, I0, beta, sigma, gamma};
+    const size_t n_strata = dust2::r::read_size(pars, "n_strata", 4);
+    return shared_state{N, I0, beta, sigma, gamma, n_strata};
   }
 
   /// @brief Updated shared parameters.
   /// @param pars A list of parameters passed from R.
   /// @param shared A shared parameter object to update.
-  static void update_shared(cpp11::list pars, shared_state& shared) {
+  static void update_shared(cpp11::list pars, shared_state &shared) {
     shared.I0 = dust2::r::read_real(pars, "I0", shared.I0);
     shared.beta = dust2::r::read_real(pars, "beta", shared.beta);
     shared.sigma = dust2::r::read_real(pars, "sigma", shared.sigma);
     shared.gamma = dust2::r::read_real(pars, "gamma", shared.gamma);
+    shared.n_strata = dust2::r::read_real(pars, "n_strata", shared.n_strata);
   }
 
   /// @brief Return incidence data -- unclear purpose.
   /// @param r_data A list of R data to copy.
   /// @param shared Shared parameters -- unclear purpose.
   /// @return Data on incidence -- unclear purpose.
-  static data_type build_data(cpp11::list r_data, const shared_state& shared) {
+  static data_type build_data(cpp11::list r_data, const shared_state &shared) {
     auto data = static_cast<cpp11::list>(r_data);
     auto incidence = dust2::r::read_real(data, "incidence", NA_REAL);
     return data_type{incidence};
@@ -79,16 +92,16 @@ public:
   /// @param internal Internal state object - not used. Purpose unclear.
   /// @param rng_state RNG state -- not used. Purpose unclear.
   /// @param state_next Next state as double value.
-  static void initial(real_type time,
-                      const shared_state& shared,
-                      internal_state& internal,
-                      rng_state_type& rng_state,
-                      real_type * state_next) {
-    state_next[0] = shared.N - shared.I0;  // S
-    state_next[1] = shared.I0;  // E
-    state_next[2] = 0.0;  // I
-    state_next[3] = 0.0;  // R
-    state_next[4] = 0.0;  // incidence
+  static void initial(real_type time, const shared_state &shared,
+                      internal_state &internal, rng_state_type &rng_state,
+                      real_type *state_next) {
+    // MAP AN EIGEN CONTAINER TO STATE_NEXT AND MODIFY CONTAINER VALUES
+    state_next[0] = shared.N - shared.I0; // S
+    state_next[1] = shared.N - shared.I0; // S
+    state_next[2] = shared.I0;            // E
+    state_next[3] = 0.0;                  // I
+    state_next[4] = 0.0;                  // R
+    state_next[5] = 0.0;                  // incidence
   }
 
   /// @brief RHS of the ODE model.
@@ -97,11 +110,11 @@ public:
   /// @param shared Shared parameters.
   /// @param internal Internal state -- purpose unclear.
   /// @param state_deriv State change or dX.
-  static void rhs(real_type time,
-                  const real_type * state,
-                  const shared_state& shared,
-                  internal_state& internal,
-                  real_type * state_deriv) {
+  static void rhs(real_type time, const real_type *state,
+                  const shared_state &shared, internal_state &internal,
+                  real_type *state_deriv) {
+    // MAP EIGEN CONTAINER TO STATE AND D_STATE and read and modify
+    // as in regular Eigen usage
     const auto S = state[0];
     const auto E = state[1];
     const auto I = state[2];
@@ -113,29 +126,37 @@ public:
     state_deriv[2] = rate_EI - rate_IR;
     state_deriv[3] = rate_IR;
     state_deriv[4] = rate_EI;
+
+    // random calculation using cpp11eigen that goes nowhere
+    // Eigen::Array<double, 10, 1> x;
+    // x.setZero();
+
+    // try mapping to state
+    // NOTE: needs to be mapped to const and needs state
+    Eigen::Map<const Eigen::VectorXd> y(state, 5);
+    // y.setZero();
   }
 
   /// @brief Set every value to zero - unclear.
   /// @param shared Shared state -- unused.
   /// @return Probably an array of zeros.
-  static auto zero_every(const shared_state& shared) {
+  static auto zero_every(const shared_state &shared) {
     return dust2::zero_every_type<real_type>{{1, {3}}};
   }
 
   /// @brief Unclear what this does.
-  /// @param time 
-  /// @param state 
-  /// @param data 
-  /// @param shared 
-  /// @param internal 
-  /// @param rng_state 
-  /// @return 
-  static real_type compare_data(const real_type time,
-                                const real_type * state,
-                                const data_type& data,
-                                const shared_state& shared,
-                                internal_state& internal,
-                                rng_state_type& rng_state) {
+  /// @param time
+  /// @param state
+  /// @param data
+  /// @param shared
+  /// @param internal
+  /// @param rng_state
+  /// @return
+  static real_type compare_data(const real_type time, const real_type *state,
+                                const data_type &data,
+                                const shared_state &shared,
+                                internal_state &internal,
+                                rng_state_type &rng_state) {
     const auto incidence_observed = data.incidence;
     if (std::isnan(incidence_observed)) {
       return 0;
